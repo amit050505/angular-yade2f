f795d813684b3a2b9c9ee731e7c9aad108577235
amit050505 <verma.amit0407@gmail.com>
2019-10-31 11:56:49 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule, HttpClient, HttpHandler } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { DataService } from './data.service.ts'
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ HttpClientModule, HttpClient, HttpHandler, DataService]
 })
 export class AppModule { }
+ 
