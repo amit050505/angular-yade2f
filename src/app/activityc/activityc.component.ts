5bade56c626d8b70a02460b9e1857ce266d5f3f6
amit050505 <verma.amit0407@gmail.com>
2019-11-01 16:54:33 +0530

    2 files modified

--- src/app/activityc/activityc.component.ts
+++ src/app/activityc/activityc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-activityc',
@@ -6,34 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activityc.component.css']
 })
 export class ActivitycComponent implements OnInit {
+
   userId = 7866;  
   articleData: any;
-  likesByMe = 0;
+  likesByMe: any;
 
-  constructor() { 
-    this.getArticle();
+  constructor(public dataService: DataService) { 
+    
+    
   }
 
   ngOnInit() {
-    this.getLikesMadeByMe(7866);
+    this.getArticle();
+    this.likesByMe = [];
   }
 
   getArticle() {
     this.articleData = JSON.parse(localStorage.getItem("articleData"));
+    this.getLikesMadeByMe(7866);
   }
 
   getLikesMadeByMe(userId) {
      let counter = 0;
-        console.log(userId);
-          this.articleData.forEach(function (art) {   
+     this.likesByMe = this.getcount();
+     console.log(this.likesByMe.length);
+  }
+
+  getcount() {
+    let arr = [];
+    this.articleData.forEach(function (art) {   
             if(art.likes.some(item => item.authorId == 7866))
             { 
-              console.log('contains');
-              this.likesByMe++;
-              counter++;
+              // console.log('contains');
+              arr.push(art);
             }
           });
-          console.log(this.likesByMe);
+          // console.log(arr);
+          return arr;
   }
 
 }
